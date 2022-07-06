8ec3353ac30c8fcf6fdfb61808411d45e27efc78
Snowkids-code <oumaj47@gmail.com>
2022-07-06 19:06:12 +0300

    Update user using secure jwt

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,30 @@
+const User = require("../models/User");
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+//Update
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.CRYPTO_PHRASE
+    ).toString();
+  }
 
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 //export router to use it
 module.exports = router;
