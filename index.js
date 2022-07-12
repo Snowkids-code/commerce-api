6d0ae083e180489792d99fb3ea3cb4b91b5f2d58
Snowkids-code <oumaj47@gmail.com>
2022-07-12 07:05:05 +0300

    add stripe function

--- index.js
+++ index.js
@@ -15,8 +15,12 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 //order route
 const orderRoute = require("./routes/order");
+//stripe route
+const stripeRoute = require("./routes/stripe");
 //configure dotenv
 dotenv.config();
+
+const cors = require("cors");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -26,12 +30,16 @@ mongoose
 //allow app to take json data
 app.use(express.json());
 
+//use cors
+app.use(cors());
+
 //routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
-app.use("/api/orders", orderRoute)
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("Backend server running");
